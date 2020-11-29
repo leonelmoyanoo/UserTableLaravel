bab79a95bbca084dca7a0edc094ee0e64c1b4a41
Leonel <leonelmoyano1809@gmail.com>
2020-11-29 14:49:44 -0300

    UserController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* GET */
+
+Route::get('/', [UserController::class, 'index']);
+
+/* POST */
+Route::post('users', [UserController::class, 'store'])
+    ->name('users.store');
+
+/* DELETE */
+Route::delete('users/{user}', [UserController::class, 'eliminar'])
+    ->name('users.destroy');
